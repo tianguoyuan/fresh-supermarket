9416058ba4182ef1e186e5e9e69df9ea17938117
tianguoyuan <603660895@qq.com>
2024-09-24 09:26:11 +0800

    发布帖子导航栏

--- src/pages/add/index.vue
+++ src/pages/add/index.vue
@@ -3,7 +3,16 @@
 
 <template>
   <div>
-    add
+    <van-nav-bar placeholder fixed title="发布帖子">
+      <template #left>
+        <RouterLink replace to="/home">
+          <van-icon name="arrow-left" color="#0B1526" />
+        </RouterLink>
+      </template>
+      <template #right>
+        <van-icon name="question-o" color="#B1B4C3" />
+      </template>
+    </van-nav-bar>
   </div>
 </template>
 
@@ -13,5 +22,5 @@
 
 <route lang="yaml">
   meta:
-    layout: home
+    title: 发布帖子
 </route>
