0e2fc3fd091c395f293e8e5e9dfd13733e02c796
tianguoyuan <603660895@qq.com>
2024-12-20 20:47:23 +0800

    分享海报功能完善

--- src/components/ShareSheet.vue
+++ src/components/ShareSheet.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { useQRCode } from '@vueuse/integrations/useQRCode'
-import { showDialog, showToast } from 'vant'
-import { copyText, openQQHref } from '~/utils'
+import html2canvas from 'html2canvas'
+import { showDialog, showLoadingToast, showToast } from 'vant'
+import { copyText, generateRandomIntegerString, openQQHref } from '~/utils'
 
 const props = withDefaults(defineProps<{
   color?: string
@@ -27,7 +28,7 @@ const options = [
 ]
 const qrcode = useQRCode(window.location.href)
 
-function onSelect({ icon }: { icon: string }) {
+async function onSelect({ icon }: { icon: string }) {
   const key = icon
   if (key === 'link') {
     copyText(window.location.href)
@@ -35,6 +36,34 @@ function onSelect({ icon }: { icon: string }) {
   }
   else if (key === 'qrcode') {
     showDialog({ message: `<img class="m-auto" src='${qrcode.value}'' />`, allowHtml: true, theme: 'round-button' })
+  }
+  else if (key === 'poster') {
+    const toastInstance = showLoadingToast({
+      mask: true,
+      message: '加载中...',
+      teleport: '#otherContainer',
+    })
+    showShare.value = false
+    await nextTick()
+    const canvas = await html2canvas(document.querySelector('#app') as HTMLElement, { useCORS: true, allowTaint: true, scrollY: 0, scrollX: 0 })
+    const dataUrl = canvas.toDataURL()
+    toastInstance.close()
+    showDialog({
+      message: `<img class="m-auto" src='${dataUrl}'' />`,
+      allowHtml: true,
+      theme: 'round-button',
+      showCancelButton: true,
+      confirmButtonText: '保存图片',
+    }).then(() => {
+      // 创建一个 a 标签，并设置 href 和 download 属性
+      const el = document.createElement('a')
+      // 设置 href 为图片经过 base64 编码后的字符串，默认为 png 格式
+      el.href = dataUrl
+      el.download = generateRandomIntegerString(10)
+      // 创建一个点击事件并对 a 标签进行触发
+      const event = new MouseEvent('click')
+      el.dispatchEvent(event)
+    })
   }
   else {
     openQQHref()
@@ -44,14 +73,7 @@ function onSelect({ icon }: { icon: string }) {
 
 <template>
   <SvgIcon icon-class="share" :color="props.color" :size="props.size" @click="showShare = !showShare" />
-  <van-share-sheet
-    v-model:show="showShare"
-    :options="options"
-    teleport="body"
-    @select="onSelect"
-  />
+  <van-share-sheet v-model:show="showShare" :options="options" teleport="body" @select="onSelect" />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
